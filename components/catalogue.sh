c1f03a97e1a2e752454ff2c6e0ed70eb3c7282cf
phani <phanendrasai41@gmail.com>
2022-07-14 19:23:59 +0530

    updating ./roboshop-shell-script

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -1,12 +1,12 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+curl -s -L -o /tmp/catalogue.zip https://github.com/roboshop-devops-project/catalogue/archive/main.zip
 cd /home/roboshop
 rm -rf catalogue
 unzip /tmp/catalogue.zip
 mv catalogue-main catalogue
-cd /home/roboshop/catalogue
+cd /catalogue
 npm install
 sed -i -e '/s/MONGO_DNSNAME/mongodb-1.roboshop.internal/' /home/roboshop/catalogue/systemd.service
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
