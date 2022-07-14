99dbf6b80d07fa20cae7b12d40bbee96fe46098a
phani <phanendrasai41@gmail.com>
2022-07-14 17:17:43 +0530

    updating ./roboshop-shell-script

--- components/frontend.sh
+++ components/frontend.sh
@@ -6,6 +6,9 @@ cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+sed -i -e '/catalogue/ s/localhost/catalogue1.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+systemctl restart nginx
+
 
 
 
