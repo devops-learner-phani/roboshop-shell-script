a6e029d15190023dc68c4f987cc71c346f79cec8
phani <phanendrasai41@gmail.com>
2022-07-14 13:08:15 +0530

    updating ./roboshop-shell-script

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -3,10 +3,8 @@ yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
 systemctl status mongod 
-
-vim /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl restart mongod
-
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 cd /tmp
 unzip -o mongodb.zip
