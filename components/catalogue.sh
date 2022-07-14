6df4ec67083d46f186c374b96150ee129dbd4158
phani <phanendrasai41@gmail.com>
2022-07-14 19:20:31 +0530

    updating ./roboshop-shell-script

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -1,3 +1,6 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
 cd /home/roboshop
 rm -rf catalogue
