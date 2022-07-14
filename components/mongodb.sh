3dda2a0fa7e0de384cd324b8d7ab7faa11e23143
phani <phanendrasai41@gmail.com>
2022-07-14 18:48:12 +0530

    updating ./roboshop-shell-script

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -1,1 +1,14 @@
-echo Install Mongodb Component
+#!/bin/bash
+
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+yum install mongodb-org -y
+systemctl start mongod
+systemctl enable mongod
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+systemctl restart mongod
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+cd /tmp
+unzip mongodb.zip
+cd mongodb-main
+mongo < catalogue.js
+mongo < users.js
