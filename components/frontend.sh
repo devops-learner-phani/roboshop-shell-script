f29f8215a7f0631b7d53d5bfc88695f28d62192f
phani <phanendrasai41@gmail.com>
2022-07-14 11:05:22 +0530

    updating ./roboshop-shell-script

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,1 +1,12 @@
-echo Install Frontend Component
+#1/bin/bash
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/static/* .
+mv frontend-main/localhost.conf /etc/nginx/default.d/conf
+
+
