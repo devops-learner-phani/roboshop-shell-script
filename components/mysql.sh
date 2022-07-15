e22ae9836f301532c92661190857dcd10735d027
phani <phanendrasai41@gmail.com>
2022-07-15 12:43:28 +0530

    updating ./roboshop-shell-script

--- components/mysql.sh
+++ components/mysql.sh
@@ -2,9 +2,9 @@ curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/robo
 yum install mysql-community-server -y
 systemctl start mysqld
 systemctl enable mysqld
-#MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-#echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" >/tmp/mysql
-#mysql -uroot -p"${MYSQL_PASSWORD}" </tmp/mysql
+MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" >/tmp/mysql
+mysql -uroot -p"${MYSQL_DEFAULT_PASSWORD}" </tmp/mysql
 #grep temp /var/log/mysqld.log
 #mysql_secure_installation
 #mysql -uroot -pRoboShop@1
