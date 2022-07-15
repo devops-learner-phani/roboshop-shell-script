532bd14aa55b9916c7a775558d5e89ac22ddd74c
phani <phanendrasai41@gmail.com>
2022-07-15 17:39:23 +0530

    updating ./roboshop-shell-script

--- components/dispatch.sh
+++ components/dispatch.sh
@@ -1,1 +1,16 @@
-echo Install Dispatch Component
+yum install golang -y
+useradd roboshop
+curl -s -L -o /tmp/dispatch.zip https://github.com/roboshop-devops-project/dispatch/archive/main.zip
+cd /home/roboshop
+rm -rf dispatch
+unzip /tmp/dispatch.zip
+mv dispatch-main dispatch
+cd dispatch
+go mod init dispatch
+go get
+go build
+sed -i -e 's/RABBITMQ-IP/rabbitmq-1.roboshop.internal/' /home/roboshop/shipping/systemd.service
+mv /home/roboshop/dispatch/systemd.service /etc/systemd/system/dispatch.service
+systemctl daemon-reload
+systemctl enable dispatch
+systemctl restart dispatch
