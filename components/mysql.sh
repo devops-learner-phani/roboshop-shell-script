61c35489f0c04606130e9162761c46f6011ee5bb
phani <phanendrasai41@gmail.com>
2022-07-15 13:53:46 +0530

    updating ./roboshop-shell-script

--- components/mysql.sh
+++ components/mysql.sh
@@ -1,3 +1,7 @@
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+yum install mysql-community-server -y
+systemctl start mysqld
+systemctl enable mysqld
 
 
 
