a0559dade670bffaca8a15fa656ea819df6a2623
phani <phanendrasai41@gmail.com>
2022-07-19 19:02:56 +0530

    updating ./roboshop-shell-script

--- components/redis.sh
+++ components/redis.sh
@@ -4,11 +4,11 @@ CHECK_ROOT
 
 PRINT "Configure yum repos"
 curl -s -o /etc/yum.repos.d/redis.repo https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo &>>${LOG}
-CHECK_STAT &?
+CHECK_STAT $?
 
 PRINT "Install redis"
 yum install redis-6.2.7 -y &>>${LOG}
-CHECK_STAT &?
+CHECK_STAT $?
 
 PRINT "Configure Mongodb service"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
