bdd8aa63d8f2dc01fa649177db0e6f5ddeef4e1b
phani <phanendrasai41@gmail.com>
2022-07-19 16:47:17 +0530

    updating ./roboshop-shell-script

--- components/common.sh
+++ components/common.sh
@@ -53,7 +53,7 @@ APP_COMMON_SETUP() {
 
 SYSTEMD() {
     PRINT "Update system congifuration"
-    sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>${LOG}
+    sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>${LOG}
     CHECK_STAT $?
 
     PRINT "Setup system configuration"
@@ -146,5 +146,29 @@ MAVEN() {
   CHECK_STAT $?
 
   SYSTEMD
+
+}
+
+PYTHON() {
+  CHECK_ROOT
+
+  PRINT "Install Python"
+  yum install python36 gcc python3-devel -y
+  CHECK_STAT $?
+
+  APP_COMMON_SETUP
+
+  PRINT "Download Python dependencies"
+  pip3 install -r requirements.txt
+  CHECK_STAT $?
+
+  USER_ID=$(id -u roboshop)
+  GROUP_ID=$(id -g roboshop)
+
+  PRINT "Update ${COMPONENT} configuration"
+  sed -i -e "/^uid/ c uid = ${USER_ID}" -e "/^gid/ c gid = ${GROUP_ID}" /home/roboshop/${COMPONENT}/${COMPONENT}.ini
+  CHECK_STAT $?
+
+  SYSTEMD 
 
 }
