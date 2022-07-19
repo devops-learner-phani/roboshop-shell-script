36d1d94be13bd27138fef980bfc96e300ce80da8
phani <phanendrasai41@gmail.com>
2022-07-19 14:28:38 +0530

    updating ./roboshop-shell-script

--- components/common.sh
+++ components/common.sh
@@ -67,7 +67,7 @@ NODEJS() {
   CHECK_STAT $?
 
   PRINT "Update systemd file configuration"
-  sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/'  /home/roboshop/${COMPONENT}/systemd.service
+  sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'  /home/roboshop/${COMPONENT}/systemd.service
   CHECK_STAT $?
 
   PRINT "Setup systemd configuration"
