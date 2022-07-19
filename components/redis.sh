ebbc1b7e0ba91d5c382a546fa1f684993eb0dddd
phani <phanendrasai41@gmail.com>
2022-07-19 19:00:14 +0530

    updating ./roboshop-shell-script

--- components/redis.sh
+++ components/redis.sh
@@ -2,8 +2,18 @@ source components/common.sh
 
 CHECK_ROOT
 
-curl -s -o /etc/yum.repos.d/redis.repo https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo
-yum install redis-6.2.7 -y
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
-systemctl enable redis
-systemctl start redis
+PRINT "Configure yum repos"
+curl -s -o /etc/yum.repos.d/redis.repo https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo &>>${LOG}
+CHECK_STAT &?
+
+PRINT "Install redis"
+yum install redis-6.2.7 -y &>>${LOG}
+CHECK_STAT &?
+
+PRINT "Configure Mongodb service"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
+CHECK_STAT $?
+
+PRINT "restart redis server"
+systemctl enable redis &>>${LOG} && systemctl restart redis &>>${LOG}
+CHECK_STAT $?
