413ce236aa6ec2390eda006d541eba6b71ddc5dd
phani <phanendrasai41@gmail.com>
2022-09-11 11:18:36 +0530

    updating .

--- components/redis.sh
+++ components/redis.sh
@@ -1,5 +1,19 @@
-curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
-yum install redis-6.2.7 -y
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
-systemctl enable redis
-systemctl start redis
+source components/common.sh
+
+CHECK_ROOT
+
+PRINT "Configure yum repos"
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Install REDIS"
+yum install redis-6.2.7 -y &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Update redis listening address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Start redis"
+systemctl enable redis &>>${LOG} && systemctl restart redis &>>${LOG}
+CHECK_STAT $?
