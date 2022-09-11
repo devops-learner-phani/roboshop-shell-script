a84e4aae871a368b0e2e60e9c226adc66d872574
phani <phanendrasai41@gmail.com>
2022-09-11 07:18:43 +0530

    updating .

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -2,6 +2,7 @@ curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/robos
 yum install -y mongodb-org
 systemctl enable mongod
 systemctl start mongod
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl restart mongod
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 cd /tmp
